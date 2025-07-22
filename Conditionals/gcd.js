75ca13d9874d46c453a798242a5180e2eb12e631
ParmS-Musale <musaleparm9541@gmail.com>
2025-07-22 16:53:37 +0530

    Solved GCD  Problem..!!

---
+++ Conditionals/gcd.js
@@ -0,0 +1,11 @@
+const greatestCommonDivisor = function(a,b){
+    let great = 1
+    for (let i=2 ; i< Math.min(a,b) ; i++){
+        if(a % i == 0 && b % i == 0){
+            great = i ;
+        } 
+    }
+    return great;
+}
+
+console.log(greatestCommonDivisor(18,15));
